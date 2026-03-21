90bdf595ced448d75bfa09c54b4ebce1b6235d77
swiftchai <lsing98@wgu.edu>
2026-03-20 18:08:28 -0700

    Add .gitignore and remove build/IDE files

--- app/src/main/java/com/example/mytravelapplication/UI/MainActivity.java
+++ app/src/main/java/com/example/mytravelapplication/UI/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mytravelapplication.UI;
 
+//TODO - remove hard coding, put datestamp into report
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
